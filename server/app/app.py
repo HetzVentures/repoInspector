4062952f407eebe3a2417b55a2d1614af27c055a
Akiva Berger <akiva10b@gmail.com>
2022-11-08 12:04:30 +0200

    procfile

--- server/app/app.py
+++ server/app/app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
